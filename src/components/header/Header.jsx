7941214c528eb90f20ecf64e7b6b00cdb9fe5642
LJ <louiejoshualabata@gmail.com>
2023-05-23 16:39:22 -0700

    cta added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header> {/* General Container Class (".container") + BIM Naming Convention ("header__container") */}
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h2>Louie Labata</h2>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
