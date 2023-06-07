831637c88371f4c04e888e52e4341f92ed72edb4
LJ <louiejoshualabata@gmail.com>
2023-06-07 10:04:17 -0700

    Finished tutorial code. Now just need to customize for myself

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {FaFacebookF} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section >
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF /></a>
+        <a href="https://instagram.com"><AiOutlineInstagram /></a>
+        <a href="https://linkedin.com"><AiOutlineLinkedin /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. ALL rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
